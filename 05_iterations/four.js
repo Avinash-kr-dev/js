79992e33e6b7835f301c48e3d089816096f2043b
Avinash <avinashkumardev.01@gmail.com>
2026-08-10 00:17:21 +0000

    for-of, for-in & Map

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: 'Javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+let programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+// map.set('IN', "India")
+
+
+// for (const keys in map) {
+//     console.log(keys);
+// }
+
