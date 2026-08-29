8d547854d3d9fbe38f1d7f3afa67adfaa4a3325b
Avinash <307091656+Avinash-kr-dev@users.noreply.github.com>
2026-08-29 20:29:44 +0000

    call

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,19 @@
+function SetUserName(username) {
+    // Complex DB calls
+    this.username = username
+    console.log("called");
+    
+}
+
+
+function createuser(username, email, password){
+    SetUserName.call(this, username)
+
+    this.email = email
+    this.password = password
+}
+
+
+const chai = new createuser("chai", "chai@fb.com", "123")
+
+console.log(chai);
